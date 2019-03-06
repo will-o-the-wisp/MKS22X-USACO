1bcce20c3a86c1a4f5336e63a3b23e4be9ab088e
will-o-the-wisp <kli18@stuy.edu>
2019-03-06 10:46:17 -0500

    wrote scanner input for first line and elevations

--- USACO.java
+++ USACO.java
@@ -1,7 +1,50 @@
 import java.io.*;
 import java.util.*;
 public class USACO{
-  public static int bronze(String filename){
+  public static void main(String[] args){
+    try{
+      bronze("makelake.1.in");
+    }
+    catch(FileNotFoundException e){
 
+    }
+  }
+  private static String arrayToString(char[][] ary){
+    String ans="";
+    for(int i=0;i<ary.length;i++){
+      for(int j=0;j<ary[0].length;j++){
+        if(ary[i][j]!='\n'){
+          ans+=ary[i][j];
+        }
+        else{
+          ans+='n';
+          ans+=ary[i][j];
+        }
+      }
+      ans+='\n';
+    }
+    return ans;
+  }
+  public static int bronze(String filename) throws FileNotFoundException{
+    int r;
+    int c;
+    int e;
+    int n;
+    int[][] lake;
+    int[][] ins;
+    File text = new File(filename);
+    Scanner inf = new Scanner(text);
+    r = inf.nextInt();
+    c = inf.nextInt();
+    e = inf.nextInt();
+    n = inf.nextInt();
+    lake = new int[r][c];
+    for(int i=0;i<r;i++){
+      for(int j=0;j<c;j++){
+        int[i][j]=inf.nextInt();
+      }
+    }
+    System.out.println(arrayToString(lake));
+    return 0;
   }
 }
